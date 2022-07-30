ad20a7f96515a1c029784e5c8028dfbdb440ff2b
noy <noyhason3@gmail.com>
2022-07-31 01:35:23 +0300

    added path to cors

--- const.ts
+++ const.ts
@@ -1,4 +1,4 @@
 export const moviesTitles = ['The Godfather', 'The Shawshank Redemption', 'Avengers: Endgame', 'Inception', 'Goodfellas', 'Gladiator', 'se7en', 'Alive', 'Thor: The Dark World', 'Game Of Thrones', 'What Women Want', 'fan']
-export const whitelist = ["http://localhost:3000"]
+export const whitelist = ["http://localhost:3000","https://my-movie-app-client.herokuapp.com/"]
 export const port = 3333;
 export const API_KEY = 'e8809c3f'
